2a5779ebefc97dc0cee4efc12299b684ea81eb9b
vinicius emanuel <vinicius.emanuel@muchmore.digital>
2018-07-18 13:15:34 -0300

    pegando location do user e usando em JS

--- webViewTest/controllers/ViewController.swift
+++ webViewTest/controllers/ViewController.swift
@@ -8,14 +8,34 @@
 
 import UIKit
 import JavaScriptCore
+import CoreLocation
 
-class ViewController: UIViewController, UIWebViewDelegate {
+class ViewController: UIViewController {
     @IBOutlet var webView: UIWebView!
     let string = "isso é um teste 2"
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.webView.delegate = self
+        self.locationManager.delegate = self
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.distanceFilter = kCLDistanceFilterNone
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            print("request de localização")
+            self.locationManager.requestAlwaysAuthorization()
+            self.locationManager.startUpdatingLocation()
+        case .authorizedAlways:
+            self.locationManager.startUpdatingLocation()
+        case .authorizedWhenInUse:
+            self.locationManager.startUpdatingLocation()
+        default:
+            print("default")
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -29,28 +49,26 @@ class ViewController: UIViewController, UIWebViewDelegate {
     @IBAction func buttonPressed(_ sender: Any) {
         webView.stringByEvaluatingJavaScript(from: "changeName('\(string)')")
     }
-    
+}
+
+extension ViewController: UIWebViewDelegate{
     func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         NSLog("request: \(request)")
-        
-        if let scheme = request.url?.scheme {
-            if scheme == "mike" {
-                // now we can react
-                print("we got a mike request: \(scheme)")
-                
-                if let result = webView.stringByEvaluatingJavaScript(from: "MIKE.someJavascriptFunc()") {
-                    print("result: \(result)")
-                }else{
-                    print("veio nada")
-                }
-                return false
-            }
-        }
+//        if let scheme = request.url?.scheme {}
         return true
     }
     
     func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
         print("error message: \(error.localizedDescription)")
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations.last?.coordinate.latitude, locations.last?.coordinate.longitude)
+//        webView.stringByEvaluatingJavaScript(from: "changeName('\(string)')")
+        guard let lat = locations.last?.coordinate.latitude, let lon = locations.last?.coordinate.longitude else{ return }
+        webView.stringByEvaluatingJavaScript(from: "changeLocation('\(lat)','\(lon)')")
     }
 }
 
